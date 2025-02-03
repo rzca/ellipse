72ab00cbee7e693d00ab15ecd5b0baece89a5f00
rzca <142755153+rzca@users.noreply.github.com>
2025-02-02 20:08:45 -0500

    tidy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import {
   Chart as ChartJS,
@@ -30,16 +28,18 @@ interface IPoint {
 }
 
 function App() {
-  const [foci1, setFoci1] = useState<IPoint>({x: 0, y: 1});
-  const [foci2, setFoci2] = useState<IPoint>({x: 7, y: 3});
+  const [foci1, setFoci1] = useState<IPoint>({ x: 0, y: 1 });
+  const [foci2, setFoci2] = useState<IPoint>({ x: 7, y: 3 });
   const [range, setRange] = useState<number>(12);
 
   // const foci1 = { x: 2, y: -2 };
   // const foci2 = { x: 7, y: 2};
   // const range = 8;
 
-  const distance = Math.sqrt((Math.pow(foci2.x - foci1.x, 2)) + (Math.pow(foci2.y - foci1.y, 2)))
-  console.log("distance", distance)
+  const distance = Math.sqrt(
+    Math.pow(foci2.x - foci1.x, 2) + Math.pow(foci2.y - foci1.y, 2)
+  );
+  console.log("distance", distance);
 
   const center = { x: (foci1.x + foci2.x) / 2, y: (foci1.y + foci2.y) / 2 };
   const slope = (foci2.y - foci1.y) / (foci2.x - foci1.x); // rise over run
@@ -47,8 +47,8 @@ function App() {
   console.log("slope", slope);
 
   // (the angle of rotation)
-  const rad = Math.atan(slope)
-  console.log("rad", rad)
+  const rad = Math.atan(slope);
+  console.log("rad", rad);
 
   const a = range / 2; // maxX
 
@@ -57,14 +57,13 @@ function App() {
   // we know that a is range / 2
   // we know that 2c is the distance between the foci
   // a^2 - b^2 = c^2
-  console.log("a", a)
+  console.log("a", a);
 
   const bsquared = Math.pow(a, 2) - Math.pow(distance / 2, 2);
   const b = Math.sqrt(bsquared);
-  console.log("b", b)
+  console.log("b", b);
   // a
   // const b = 1; // maxY
-
 
   // set x to 0, solve for y
 
@@ -79,7 +78,7 @@ function App() {
 
   const xys = xs.map((x) => ({
     x,
-    y: b * Math.sqrt(1 - ((Math.pow(x, 2))) / (a * a)),
+    y: b * Math.sqrt(1 - Math.pow(x, 2) / (a * a)),
   }));
 
   const negs = xys.map(({ x, y }) => ({ x, y: -y }));
@@ -96,19 +95,25 @@ function App() {
     y: y + center.y,
   }));
 
-
-  const dist = (point: {x: number, y: number}, foci1: {x: number, y: number}, foci2: {x: number, y: number} ) => {
-    const dist1 =  Math.sqrt((Math.pow(point.x - foci1.x, 2)) + (Math.pow(point.y - foci1.y, 2)))
-    const dist2 =  Math.sqrt((Math.pow(point.x - foci2.x, 2)) + (Math.pow(point.y - foci2.y, 2)))
+  const dist = (
+    point: { x: number; y: number },
+    foci1: { x: number; y: number },
+    foci2: { x: number; y: number }
+  ) => {
+    const dist1 = Math.sqrt(
+      Math.pow(point.x - foci1.x, 2) + Math.pow(point.y - foci1.y, 2)
+    );
+    const dist2 = Math.sqrt(
+      Math.pow(point.x - foci2.x, 2) + Math.pow(point.y - foci2.y, 2)
+    );
     return dist1 + dist2;
-  }
+  };
 
   for (const pt of all_rotated_moved) {
     if (!Number.isNaN(dist(pt, foci1, foci2))) {
       console.log(pt, dist(pt, foci1, foci2));
     }
   }
-
 
   const chart = (
     <Scatter
@@ -162,11 +167,65 @@ function App() {
     <>
       <h1>Solving an ellipse</h1>
       {<div style={{ width: "600px", height: "600px" }}>{chart}</div>}
-      <input type="number" id="distance" name="distance" min="0" max="30" onChange={(e) => setRange(parseFloat(e.target.value))}/>
-      <input type="number" id="foci1-x" name="foci1 - x" min="-20" max="15" onChange={(e) => setFoci1({...foci1, x: parseFloat(e.target.value)})}/>
-      <input type="number" id="foci1-y" name="foci1 - y" min="-10" max="10" onChange={(e) => setFoci1({...foci1, y: parseFloat(e.target.value)})}/>
-      <input type="number" id="foci2-x" name="foci2 - x" min="-20" max="15" onChange={(e) => setFoci1({...foci2, x: parseFloat(e.target.value)})}/>
-      <input type="number" id="foci2-y" name="foci2 - y" min="-10" max="10" onChange={(e) => setFoci1({...foci2, y: parseFloat(e.target.value)})}/>
+
+      <div style={{ display: "flex", flexDirection: "column", width: "50px" }}>
+        {"Distance"}
+        <input
+          type="number"
+          placeholder="12"
+          id="distance"
+          name="distance"
+          min="0"
+          max="30"
+          onChange={(e) => setRange(parseFloat(e.target.value))}
+        />
+        {"Foci 1"}
+        <input
+          type="number"
+          placeholder="x"
+          id="foci1-x"
+          name="foci1 - x"
+          min="-20"
+          max="15"
+          onChange={(e) =>
+            setFoci1({ ...foci1, x: parseFloat(e.target.value) })
+          }
+        />
+        <input
+          type="number"
+          placeholder="y"
+          id="foci1-y"
+          name="foci1 - y"
+          min="-10"
+          max="10"
+          onChange={(e) =>
+            setFoci1({ ...foci1, y: parseFloat(e.target.value) })
+          }
+        />
+        {"Foci 2"}
+        <input
+          type="number"
+          placeholder="x"
+          id="foci2-x"
+          name="foci2 - x"
+          min="-20"
+          max="15"
+          onChange={(e) =>
+            setFoci2({ ...foci2, x: parseFloat(e.target.value) })
+          }
+        />
+        <input
+          type="number"
+          id="foci2-y"
+          placeholder="y"
+          name="foci2 - y"
+          min="-10"
+          max="10"
+          onChange={(e) =>
+            setFoci2({ ...foci2, y: parseFloat(e.target.value) })
+          }
+        />
+      </div>
     </>
   );
 }
